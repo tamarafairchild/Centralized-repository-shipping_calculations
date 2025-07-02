d5fab08ddc42c9ff9c1ee1826442794e4894bcad
tamarafairchild <tmf070805@gmail.com>
2025-07-02 16:01:01 -0400

    Revert "another line"
    This reverts commit 148a762b0f73f8dcc7c405fa52bf41b173155532.

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -10,5 +10,4 @@ shipping_cost = weight * rate
 ## Display the result
 print(f"Shipping Cost: {shipping_cost} USD")
 
- # Here is a new update by <tamarafairchild>
- # Here is another update by <tamarafairchild>
+ # Here is a new update by <tamarafairchild
